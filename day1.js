ff174fc4f1907bc27db8b1e2cfd5a2b85368bdef
marie <ingabiremarieaimee6@gmail.com>
2023-11-22 16:13:21 -0800

    push me

---
+++ day1.js
@@ -0,0 +1,35 @@
+console.log("2")
+const me =4;
+const you = 7;
+console.log(Boolean(me<you));
+console.log(Boolean(you<me));
+
+
+const numOne=3;
+const numThree=2;
+console.log("numOne>=numThree");
+console.log("Hello, World!");
+
+let name = "aimee";
+let age = 19;
+let school= you;
+let isStudent = true;
+
+console.log(`Name: ${name}, Age: ${age},school:${you} , Student: ${isStudent}`);
+
+
+
+
+// Using variables
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log(sum);
+
+// Changing variable values
+let message = "Hello";
+message = "Hi";
+console.log(message); 
+
+// Constants cannot be reassigned
+const gravity = 9.8;
